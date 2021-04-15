6e3569342ea098d58726ac5787db626c27514f7e
Elinan13 <elina.nersesyan@gmail.com>
2021-04-15 22:27:12 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+// function App() {
+//   return (
+//     <div className="App">
+// My first App
+//     </div>
+//   );
+// }
+//
+// export default App;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+import Header from './header';
+import Footer from './Footer';
+import {Content} from './Content';
+
+
+// const arr = ["Prod1", "Prod2", "Prod3"]
+
+class App extends Component {
+    render() {
+        return (<div>
+                <Header/>
+                <Content/>
+                <Footer/>
+                {/*{*/}
+                {/*    arr.map((item) => {*/}
+                {/*        return <div className={'product'}> {item}</div>*/}
+                {/*    })*/}
+                {/*}*/}
+                {/*<div>product 1</div>*/}
+                {/*{false &&<div>product 2</div>}*/}
+                {/*<div>product 3</div>*/}
+                {/*<div>product 4</div>*/}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
+// const App2 =()=>{
+//    return(
+//        <div>functional component</div>
+//    )
+// };
+
+
+//14.04.2021
+//linuma erku dzev, Class-ov u sovorakan function-ov, Classi depqum Extendy u Render partadir en, isk
+//functioni jamanak petq chi, funkcian partadir petqa Const-ov lini
+//functioni jamanaak karanq export anenq miangamic consti koxkic, voch te ira takic
+
+//<div>-y react-um kochvuma "JSX", html-i poxaren, inqy aveli hzora
+//return petqa ani mi hat vyorstka, voch te mi qani hat nuynic, petqa return anel mi hat divi mej
+//kodery sirunacnelu hamar "CTRL ALT L"
+//karanq voch te DIV-i mej grenq ayl datark kavichkaneri mej, orinak <> u stex tarber diver</>
+//vyorstkai mej karanq nayev grenq js code, partadir dzevavor pakagceri mej, bayc sahmanapak JS, voch te amen inch
+//if, for, while chenq kara grenq stex, vorovhetev sahmanapaka
+//i tarberutyun CSS-i stex div-in talis enq voch te Class, ayl className, u anpayman dzevavor pakagic u mejy kavichkeq
+//ay senc <div className={"first"}> </div>
+//xorhurd chi trvum div-in tal id, petqa tal className
+//ete petqa datark div lini karanqq uxxaki mi hatanoc senc grenq <div/> (orinak bac taracq toxelu hamar)
+//inline style talu hamar karanq henc js-i mej grenq voch te css-i, <div style={{backgroundColor: 'red'}}>
+//SPA- Single Page Application, vor yndameny 1 hat HTML eja
+//
+//folderneri anunnery anpayman petqa linen poqratarov, u space chunenan, petqa linen gcikov
+
+
---
+++ src/Content/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+export const Content = () => {
+    return (
+        <div>
+            This is Content
+        </div>
+    )
+};
+
+//ete functional-v enq grum componenty,, apa import aneluc anpayman petqa dzevavor pakagci mej import anenq
+//import {Content} from './content/index';
+// class Content extends Component {
+//     render() {
+//         return (
+//             <div>This is Content</div>
+//         )
+//     }
+// }
+//
+// export default Content
